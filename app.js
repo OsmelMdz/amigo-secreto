7108bde0c46c9746751d31c54de7ee591d4937b9
Osmel Mendez <84472632+OsmelMdz@users.noreply.github.com>
2025-01-24 23:39:56 -0600

    Implement function to add friends

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // Crear un array para almacenar los nombres de los amigos
 let amigos = [];
+
+// Implementa una función para agregar amigos
+function agregarAmigo() {
+    const nombre = document.getElementById('amigo').value.trim();
+    const regex = /^[A-Za-z]+(\s[A-Za-z]+)*$/;
+    if (nombre === '' || !regex.test(nombre)) {
+        alert('Por favor, ingrese un nombre válido (solo letras y espacios entre palabras)');
+        return;
+    }
+    amigos.push(nombre);
+    mostrarListaAmigos();
+    document.getElementById('amigo').value = '';
+}
